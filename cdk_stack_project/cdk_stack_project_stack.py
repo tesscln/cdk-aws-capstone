0f5557dcee703ead60111fcb1ad45c4de1177325
Tess Coullon <tessclln@amazon.fr>
2024-07-25 12:03:45 +0200

    debug cdk_stack_project_stack.py for prop['name'] string error

--- cdk_stack_project/cdk_stack_project_stack.py
+++ cdk_stack_project/cdk_stack_project_stack.py
@@ -11,6 +11,11 @@ class CdkStackProjectStack(Stack):
                  asset_properties: list, assets: list, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        print("Asset Model Properties Input:", asset_properties)
+
+        if not isinstance(asset_properties, list) or not all(isinstance(prop, dict) for prop in asset_properties):
+            raise ValueError("asset_properties must be a list of dictionaries")
+
         # Defining an IoT SiteWise asset model:
 
         asset_model_properties = [
@@ -29,6 +34,16 @@ class CdkStackProjectStack(Stack):
             asset_model_name=asset_model_name,
             asset_model_properties=asset_model_properties
         )
+
+        print("Assets Input:", assets)
+
+        if not isinstance(assets, list) or not all(isinstance(asset, dict) for asset in assets):
+            raise ValueError("assets must be a list of dictionaries")
+        
+        for asset in assets:
+            if not isinstance(asset.get('properties'), list) or not all(isinstance(prop, dict) for prop in asset['properties']):
+                raise ValueError("Each asset's properties must be a list of dictionaries")
+
         
         # Create an IoT SiteWise asset belonging to the created asset model
 
