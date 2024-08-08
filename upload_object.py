c300f80017ee05f9806fd1a57c42b9f6a34849a5
Tess Coullon <tessclln@amazon.fr>
2024-08-08 18:30:41 +0200

    committing before update computer shutdown

--- upload_object.py
+++ upload_object.py
@@ -7,12 +7,18 @@ def upload_file_to_s3(file_path, bucket_name, region):
     try:
         s3_client.upload_file(file_path, bucket_name, os.path.basename(file_path))
         print(f"File {file_path} uploaded successfully to bucket '{bucket_name}'.")
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except NoCredentialsError:
+        print("Credentials not available.")
+    except ClientError as e:
+        print(f"Client error: {e}")
     except Exception as e:
         print(f"Failed to upload file: {str(e)}")
 
 
 if __name__ == "__main__":
-    
+
     # Ask user for the file path
     file_path = input("Enter the file path to upload the 3D object (it needs to be in .usd format): ")
 
